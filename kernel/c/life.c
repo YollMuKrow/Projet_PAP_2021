eeda7a38e36dbd562bb1f2e2aa438e0ff2adcd62
Alexis Lheritier <yollmukrow@gmail.com>
2021-04-17 18:23:18 +0200

    envoi au cremi (ocl_finish)

--- kernel/c/life.c
+++ kernel/c/life.c
@@ -122,10 +122,10 @@ unsigned life_invoke_ocl_finish (unsigned nb_iter)
         err |= clSetKernelArg(compute_kernel, 2, sizeof(cl_mem), &change_buffer);
         check(err, "Failed to set kernel arguments");
 
-        printf("change mid ite %u = %u\n", it, change_buffer_value[0]);
         err = clEnqueueNDRangeKernel(queue, compute_kernel, 2, NULL, global, local,
                                      0, NULL, NULL);
         check(err, "Failed to execute kernel");
+        printf("change mid ite %u = %u\n", it, change_buffer_value[0]);
 
         {
             cl_mem tmp = cur_buffer;
