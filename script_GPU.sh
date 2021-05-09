441575b7723fc37b34dc424c02db3aaa8a6c0604
Alexis Lheritier <yollmukrow@gmail.com>
2021-05-10 01:42:36 +0200

    script GPU (à tester)

--- script_GPU.sh
+++ script_GPU.sh
@@ -3,14 +3,63 @@
 
 rm -rf plots/data/perf_data.csv
 
-for i in $(seq 1 1)
+for i in $(seq 1 5)
 do
   echo $i
-  for tileX in $(seq 0 10)
+  for tileX in $(seq 1 9)
   do
-    for tileY in $(seq 0 10)
+    for tileY in $(seq 1 9)
     do
-      TILEX=$((2**$tileX)) TILEY=$((2**$tileY)) ./run -k life -o -v ocl_finish -n -i 1000 -a meta3x3 -s 6208 -tw $((2**$tileX)) -th $((2**$tileY)) 
+      TILEX=$((2**$tileX)) TILEY=$((2**$tileY)) ./run -k life -o -v ocl_hybrid -n -i 1000 -a meta3x3 -s 6208 -tw $((2**$tileX)) -th $((2**$tileY))
     done
   done
 done
+ mv plots/data/perf_data.csv perf_meta_hybrid_tile.csv
+
+ #### calcul random
+ rm -rf plots/data/perf_data.csv
+
+for i in $(seq 1 5)
+do
+  echo $i
+  for tileX in $(seq 1 6)
+  do
+    for tileY in $(seq 1 6)
+    do
+      TILEX=$((2**$tileX)) TILEY=$((2**$tileY)) ./run -k life -o -v ocl_hybrid -n -i 1000 -a random -s 4096 -tw $((2**$tileX)) -th $((2**$tileY))
+    done
+  done
+done
+ mv plots/data/perf_data.csv perf_random_hybrid_tile.csv
+
+  #### calcul guns
+ rm -rf plots/data/perf_data.csv
+
+for i in $(seq 1 5)
+do
+  echo $i
+  for tileX in $(seq 1 9)
+  do
+    for tileY in $(seq 1 9)
+    do
+      TILEX=$((2**$tileX)) TILEY=$((2**$tileY)) ./run -k life -o -v ocl_hybrid -n -i 1000 -a guns -s 4096 -tw $((2**$tileX)) -th $((2**$tileY))
+    done
+  done
+done
+ mv plots/data/perf_data.csv perf_guns_hybrid_tile.csv
+
+   #### calcul sparse
+ rm -rf plots/data/perf_data.csv
+
+for i in $(seq 1 5)
+do
+  echo $i
+  for tileX in $(seq 1 9)
+  do
+    for tileY in $(seq 1 9)
+    do
+      TILEX=$((2**$tileX)) TILEY=$((2**$tileY)) ./run -k life -o -v ocl_hybrid -n -i 1000 -a sparse -s 4096 -tw $((2**$tileX)) -th $((2**$tileY))
+    done
+  done
+done
+ mv plots/data/perf_data.csv perf_sparse_hybrid_tile.csv
